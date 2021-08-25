9066722f07172305ee1eebbbc6e83f3622d4f1a5
jay <jayhegshetye@gmail.com>
2021-08-25 14:29:59 +0530

    worked on deskewing

--- deskewImg.py
+++ deskewImg.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
+from numpy.lib.type_check import imag
 
-img = cv2.imread(r"L:\final_year_project\test1.PNG")
+img = cv2.imread(r"L:\final_year_project\test2.PNG")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 gray = cv2.bitwise_not(gray)
 
@@ -15,10 +16,20 @@ if angle < -45:
 else:
     angle = -angle
 
+h, w, c = img.shape
 
-print(coords)
-print(angle)
-cv2.imshow("op", thresh)
+center = (w//2, h//2)
+M = cv2.getRotationMatrix2D(center, angle, 1.0)
+rotated = cv2.warpAffine(
+    img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+cv2.putText(rotated, "Angle: {:.2f} degrees".format(
+    angle), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+print("[INFO] angle: {:.3f}".format(angle))
+cv2.imshow("Input", img)
+cv2.imshow("Rotated", rotated)
+
+
 cv2.waitKey(0)
 
 cv2.destroyAllWindows()
